06429eeac06a58ad9d5b02e9606969aedf36d3f1
Jovan Jevtic <jovanjevtic132@gmail.com>
2021-11-16 17:41:39 +0100

    hamburger menu animation on click

--- js/header.js
+++ js/header.js
@@ -1,9 +1,16 @@
 const modalMenuToggle = () => {
     const btn = document.getElementById('hamburger-menu');
     const modalMenu = document.getElementsByClassName('modal-menu')[0];
+    const btnLines = document.querySelectorAll('.hamburger-menu-line');
 
     btn.addEventListener('click', () => {
         modalMenu.classList.toggle('modal-menu-active');
+        console.log(btnLines)
+
+        btnLines.forEach(el => {
+            el.classList.toggle('hamburger-menu-line-active');
+            console.log(el)
+        });
     });
 }
 
